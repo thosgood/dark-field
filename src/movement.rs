62a5fbff8c76cdfde3b6fca26772c7235e404ee2
Tim Hosgood <tim.hosgood@gmail.com>
2026-07-31 00:29:33 +0100

    starting to have some f32 stuff

--- src/movement.rs
+++ src/movement.rs
@@ -1,9 +1,43 @@
+use std::f32::consts::{FRAC_PI_2, FRAC_PI_4, PI};
+
 #[derive(Debug)]
 pub enum DiscreteDirection {
     North,
     East,
     South,
     West,
+}
+
+#[derive(Debug)]
+pub struct RealDirection(pub f32);
+
+impl RealDirection {
+    pub fn nearest_discrete_direction(&self) -> DiscreteDirection {
+        // We rotate the angle by pi/4 so that we can simply check which quadrant it
+        // lies in, rather than which axis it's closest to.
+        let (sin, cos) = (self.0 + FRAC_PI_4).sin_cos();
+        let s_cos = cos.is_sign_positive();
+        let s_sin = sin.is_sign_positive();
+        match (s_cos, s_sin) {
+            // Bottom-left quadrant => West.
+            (false, false) => DiscreteDirection::West,
+            // Top-left quadrant => North.
+            (false, true) => DiscreteDirection::North,
+            // Bottom-right quadrant => South.
+            (true, false) => DiscreteDirection::South,
+            // Top-right quadrant => East.
+            (true, true) => DiscreteDirection::East,
+        }
+    }
+
+    pub fn from_discrete_direction(direction: &DiscreteDirection) -> Self {
+        match direction {
+            DiscreteDirection::North => RealDirection(FRAC_PI_2),
+            DiscreteDirection::East => RealDirection(0f32),
+            DiscreteDirection::South => RealDirection(-FRAC_PI_2),
+            DiscreteDirection::West => RealDirection(PI),
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -35,15 +69,15 @@ pub struct RealPosition {
 }
 
 impl RealPosition {
-    pub fn from_grid_position(grid_position: &DiscretePosition) -> Self {
+    pub fn from_discrete_position(discrete_position: &DiscretePosition) -> Self {
         // TODO: check this is ok? what is the usize is too big...
         RealPosition {
-            x: grid_position.x as f32,
-            y: grid_position.y as f32,
+            x: discrete_position.x as f32,
+            y: discrete_position.y as f32,
         }
     }
 
-    pub fn nearest_grid_position(&self) -> DiscretePosition {
+    pub fn nearest_discrete_position(&self) -> DiscretePosition {
         DiscretePosition {
             x: self.x.round() as usize,
             y: self.y.round() as usize,
--- src/player.rs
+++ src/player.rs
@@ -2,11 +2,15 @@ use crate::Either;
 use crate::locations::*;
 use crate::movement::*;
 
+const WALKING_SPEED: f32 = 0.8;
+const TURNING_SPEED: f32 = 0.3;
+
 #[derive(Debug)]
 pub struct Player {
-    pub grid_position: DiscretePosition,
+    pub discrete_position: DiscretePosition,
     pub real_position: RealPosition,
-    pub direction: DiscreteDirection,
+    pub discrete_direction: DiscreteDirection,
+    pub real_direction: RealDirection,
     pub eyesight: usize,
     pub debug: String,
 }
@@ -16,23 +20,25 @@ impl Player {
         location: &Location,
         starting_x: usize,
         starting_y: usize,
-        direction: DiscreteDirection,
+        discrete_direction: DiscreteDirection,
         eyesight: usize,
     ) -> Self {
         // TODO: isn't there a slicker way to do this? surely
-        let grid_position = match location.force_can_walk_on(&DiscretePosition {
+        let discrete_position = match location.force_can_walk_on(&DiscretePosition {
             x: starting_x,
             y: starting_y,
         }) {
             Either::Left(desired_point) => desired_point,
             Either::Right(forced_point) => forced_point,
         };
-        let real_position = RealPosition::from_grid_position(&grid_position);
+        let real_position = RealPosition::from_discrete_position(&discrete_position);
+        let real_direction = RealDirection::from_discrete_direction(&discrete_direction);
 
         Player {
-            grid_position,
+            discrete_position,
             real_position,
-            direction,
+            discrete_direction,
+            real_direction,
             eyesight,
             debug: String::new(),
         }
@@ -40,22 +46,22 @@ impl Player {
 
     pub fn take_step(&mut self, map: &Location) {
         // TODO: update this to also update real_position
-        let tentative_position = match self.direction {
+        let tentative_position = match self.discrete_direction {
             DiscreteDirection::North => DiscretePosition {
-                x: self.grid_position.x,
-                y: (self.grid_position.y).saturating_sub(1),
+                x: self.discrete_position.x,
+                y: (self.discrete_position.y).saturating_sub(1),
             },
             DiscreteDirection::East => DiscretePosition {
-                x: self.grid_position.x + 1,
-                y: self.grid_position.y,
+                x: self.discrete_position.x + 1,
+                y: self.discrete_position.y,
             },
             DiscreteDirection::South => DiscretePosition {
-                x: self.grid_position.x,
-                y: self.grid_position.y + 1,
+                x: self.discrete_position.x,
+                y: self.discrete_position.y + 1,
             },
             DiscreteDirection::West => DiscretePosition {
-                x: (self.grid_position.x).saturating_sub(1),
-                y: self.grid_position.y,
+                x: (self.discrete_position.x).saturating_sub(1),
+                y: self.discrete_position.y,
             },
         };
 
@@ -67,41 +73,54 @@ impl Player {
         }
 
         if map.is_in_bounds(&tentative_position) && map.can_walk_on(&tentative_position) {
-            self.grid_position = tentative_position;
+            self.discrete_position = tentative_position;
         }
+    }
+
+    pub fn turn_left(&mut self) {
+        self.real_direction.0 += TURNING_SPEED;
+        self.discrete_direction = self.real_direction.nearest_discrete_direction();
+    }
+
+    pub fn turn_right(&mut self) {
+        self.real_direction.0 -= TURNING_SPEED;
+        self.discrete_direction = self.real_direction.nearest_discrete_direction();
     }
 
     // TODO: can't you do something like Direction.x => Direction.(x-1) ?
     //       (maybe even by just implementing Iterate? but surely simpler...)
-    pub fn turn_left(&mut self) {
-        self.direction = match self.direction {
+    pub fn turn_discrete_left(&mut self) {
+        self.discrete_direction = match self.discrete_direction {
             DiscreteDirection::North => DiscreteDirection::West,
             DiscreteDirection::East => DiscreteDirection::North,
             DiscreteDirection::South => DiscreteDirection::East,
             DiscreteDirection::West => DiscreteDirection::South,
         };
+        self.real_direction = RealDirection::from_discrete_direction(&self.discrete_direction);
     }
 
-    pub fn turn_right(&mut self) {
-        self.direction = match self.direction {
+    pub fn turn_discrete_right(&mut self) {
+        self.discrete_direction = match self.discrete_direction {
             DiscreteDirection::North => DiscreteDirection::East,
             DiscreteDirection::East => DiscreteDirection::South,
             DiscreteDirection::South => DiscreteDirection::West,
             DiscreteDirection::West => DiscreteDirection::North,
         };
+        self.real_direction = RealDirection::from_discrete_direction(&self.discrete_direction);
     }
 
     pub fn selfie(&self) -> char {
-        match self.direction {
-            DiscreteDirection::North => '⮝',
-            DiscreteDirection::East => '⮞',
-            DiscreteDirection::South => '⮟',
-            DiscreteDirection::West => '⮜',
+        match self.discrete_direction {
+            DiscreteDirection::North => '↑',
+            DiscreteDirection::East => '→',
+            DiscreteDirection::South => '↓',
+            DiscreteDirection::West => '←',
         }
     }
 
     pub fn take_photo(&self, map: &Location) -> Photo {
-        let Photo(mut photo) = map.take_photo(&(self.grid_position), self.eyesight, self.eyesight);
+        let Photo(mut photo) =
+            map.take_photo(&(self.discrete_position), self.eyesight, self.eyesight);
         // Because we specify photos as having centre points, but we access points
         // in a `Vec<Vec<_>>` by working from the top left, we always need to shift
         // by the (half) size of the photo (which, here, is given by `eyesight`).
